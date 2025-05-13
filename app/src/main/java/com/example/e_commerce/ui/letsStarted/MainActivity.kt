b82c53abaf1db83c2811867d6278e2fa5b42f67d
Pratik Patil <pratikpatil@Pratiks-MacBook-Air.local>
2025-05-13 10:17:32 +0530

    adding loginactivity screen

--- app/src/main/java/com/example/e_commerce/ui/letsStarted/MainActivity.kt
+++ app/src/main/java/com/example/e_commerce/ui/letsStarted/MainActivity.kt
@@ -1,47 +1,22 @@
 package com.example.e_commerce.ui.letsStarted
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.e_commerce.ui.theme.EcommerceTheme
+import com.example.e_commerce.R
+import com.example.e_commerce.ui.login.LoginScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContent {
-            EcommerceTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+        setContentView(R.layout.activity_main)
+
+        val button = findViewById<Button>(R.id.button)
+
+        button.setOnClickListener {
+            val intent = Intent(this, LoginScreen::class.java)
+            startActivity(intent)
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    EcommerceTheme {
-        Greeting("Android")
     }
 }
--- app/src/main/java/com/example/e_commerce/ui/login/LoginScreen.kt
+++ app/src/main/java/com/example/e_commerce/ui/login/LoginScreen.kt
@@ -1,21 +1,12 @@
 package com.example.e_commerce.ui.login
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import androidx.activity.ComponentActivity
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import com.example.e_commerce.R
 
 class LoginScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_login_screen)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.loginActivity)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
-    }
+        setContentView(R.layout.activity_login_screen) }
 }
