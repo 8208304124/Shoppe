e509ecf4e672abd1dc94d833d816ff6313cb66e6
Utkarsh Patil <utkarsh.p@upperthrust.com>
2025-05-15 16:13:21 +0530

    lets get started screen completed

--- app/src/main/java/com/example/e_commerce/ui/letsStarted/MainActivity.kt
+++ app/src/main/java/com/example/e_commerce/ui/letsStarted/MainActivity.kt
@@ -3,8 +3,12 @@ package com.example.e_commerce.ui.letsStarted
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import com.example.e_commerce.R
+import com.example.e_commerce.ui.createAccount.CreateAccount
 import com.example.e_commerce.ui.login.LoginScreen
 
 class MainActivity : ComponentActivity() {
@@ -12,11 +16,21 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val button = findViewById<Button>(R.id.button)
+        val alreadyHaveAccountTxt = findViewById<TextView>(R.id.textView4)
+        val arrowIcon = findViewById<ImageView>(R.id.imageView8)
+        val letsGetStartedBtn = findViewById<Button>(R.id.button)
 
-        button.setOnClickListener {
+        val navigateToLogin = {
             val intent = Intent(this, LoginScreen::class.java)
             startActivity(intent)
         }
+        val navigateToCreateAccount = {
+            val intent = Intent(this, CreateAccount::class.java)
+            startActivity(intent)
+        }
+        letsGetStartedBtn.setOnClickListener { navigateToCreateAccount() }
+        alreadyHaveAccountTxt.setOnClickListener { navigateToLogin() }
+        arrowIcon.setOnClickListener { navigateToLogin() }
+
     }
 }
