37fb34b74f833db66227a13e124f2a3fdc8edf1c
Utkarsh Patil <114092129+ubpatil0524@users.noreply.github.com>
2025-05-15 16:17:36 +0530

    Feature/lets get started screen & create account screen (#1)
    * adding lets get started screen
    
    * lets get started screen completed
    
    ---------
    
    Co-authored-by: Utkarsh Patil <utkarsh.p@upperthrust.com>

---
+++ app/src/main/java/com/example/e_commerce/ui/createAccount/CreateAccount.kt
@@ -0,0 +1,12 @@
+package com.example.e_commerce.ui.createAccount
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import com.example.e_commerce.R
+
+class CreateAccount: ComponentActivity()  {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_account_screen)
+    }
+}
