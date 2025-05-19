3def6e50ecf60ba0a54c2b497f2d3163c8391c25
Utkarsh Patil <114092129+ubpatil0524@users.noreply.github.com>
2025-05-19 18:51:13 +0530

    Feature/card img view slider (#5)
    * set password and password sms recovery screeen implemented
    
    * sms password screen completed
    
    * card made for img slider
    
    * card file moved to package
    
    ---------
    
    Co-authored-by: Utkarsh Patil <utkarsh.p@upperthrust.com>

--- app/src/main/java/com/example/e_commerce/ui/lets_start_cards/Lets_start_cards.kt
+++ app/src/main/java/com/example/e_commerce/ui/lets_start_cards/Lets_start_cards.kt
@@ -1,32 +1,21 @@
-package com.example.e_commerce.ui.setup_new_password
+package com.example.e_commerce.ui.lets_start_cards
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
-import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import com.example.e_commerce.R
-import com.example.e_commerce.ui.lets_start_cards.lets_start_cards
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.e_commerce.R
 
-class setup_new_password : AppCompatActivity() {
+class lets_start_cards : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_setup_new_password)
-        val button = findViewById<Button>(R.id.button2)
-        val cancelBtn = findViewById<TextView>(R.id.button3)
-
-        button.backgroundTintList = null
+        setContentView(R.layout.activity_lets_start_cards)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-        }
-        cancelBtn.setOnClickListener {
-            finish()
         }
     }
 }
